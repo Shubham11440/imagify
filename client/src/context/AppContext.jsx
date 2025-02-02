130e6756ee1466c44569afc8f22ebf17cbf03481
Shubham Mali <shubhamvmali09@gmail.com>
2025-02-02 12:09:25 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -13,7 +13,7 @@ const AppContextProvider = (props) => {
 
     const [credit, setCredit] = useState(false)
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://imagify-mu-ten.vercel.app/"
     const navigate = useNavigate()
 
     const loadCreditsData = async () => {
